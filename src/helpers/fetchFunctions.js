e944c8f16f0bb6ab05abff89834a1399d9406142
gustavomorais <gustavomorais.dev@gmail.com>
2023-02-16 19:50:41 -0300

    configura a função fetchProductsList

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,13 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (query) => {
+  if (!query) throw new Error('Termo de busca não informado');
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,15 +5,25 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    const actual = typeof fetchProductsList;
+    const expected = 'function';
+
+    expect(actual).toBe(expected);
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
+    console.log(fetchProductsList('computador'));
+    const actual = fetchProductsList('computador');
+    const expected = 'function';
 
+    expect(actual).toBe(expected);
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
+    const actual = typeof fetchProductsList;
+    const expected = 'function';
 
+    expect(actual).toBe(expected);
   });
 
   // it('...', () => {
