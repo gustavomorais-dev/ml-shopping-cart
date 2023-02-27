40a49181dfa8a21bd98a7bed8f181bb1f32b74aa
gustavomorais <gustavomorais.dev@gmail.com>
2023-02-27 20:49:41 -0300

    completa o requisito 9 que carrega o carrinho salvo em localstorage

--- src/main.js
+++ src/main.js
@@ -2,12 +2,27 @@ import { searchCep } from './helpers/cepFunctions';
 import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
 import { createCartProductElement, createProductElement } from './helpers/shopFunctions';
 import { showLoading, hideLoading } from './helpers/messageFunctions';
-import { saveCartID } from './helpers/cartFunctions';
+import { getSavedCartIDs, saveCartID } from './helpers/cartFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const productsSection = document.getElementsByClassName('products')[0];
-const cartProducts = document.getElementsByClassName('cart__products')[0];
+const cartProductsContainer = document.getElementsByClassName('cart__products')[0];
+
+const recoverCart = () => {
+  const productsIds = getSavedCartIDs();
+  const products = productsIds.map(async (id) => {
+    const details = await fetchProduct(id);
+    return details;
+  });
+  Promise.all(products).then((list) => {
+    list.forEach((productDetails) => {
+      const { id, title, price, pictures } = productDetails;
+      const newCartElement = createCartProductElement({ id, title, price, pictures });
+      cartProductsContainer.appendChild(newCartElement);
+    });
+  });
+};
 
 const addProduct = async (event) => {
   const productId = event.target.parentNode.firstChild.innerText;
@@ -15,7 +30,7 @@ const addProduct = async (event) => {
   const productDetails = await fetchProduct(productId);
   const { id, title, price, pictures } = productDetails;
   const newCartElement = createCartProductElement({ id, title, price, pictures });
-  cartProducts.appendChild(newCartElement);
+  cartProductsContainer.appendChild(newCartElement);
 };
 
 const listProducts = async () => {
@@ -31,3 +46,4 @@ const listProducts = async () => {
 };
 
 listProducts();
+recoverCart();
