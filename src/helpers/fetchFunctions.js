95421748f17f41835919ff60a5006af496c63ef3
gustavomorais <gustavomorais.dev@gmail.com>
2023-02-27 19:12:41 -0300

    completa o requisito 7, que implementa a função fetchproduct

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,16 @@
 import { showError, hideError } from './messageFunctions';
 
-export const fetchProduct = () => {
-  // seu código aqui
+export const fetchProduct = async (id) => {
+  hideError();
+  if (!id) throw new Error('ID não informado');
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    showError();
+  }
 };
 
 export const fetchProductsList = async (query) => {
